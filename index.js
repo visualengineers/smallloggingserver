828bc6fb0779ac40f416fda00fd66ab6864dde4f
dkammer <kammer@htw-dresden.de>
2021-12-22 11:16:09 +0100

    db output desc

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const app = express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('select logging_events.key, sessionid, timecode, logging_events.eventid, logging_eventcodes.description as eventdescription, logging_events.statusid, logging_statuscodes.description as statusdescription, ticketid from logging_events join logging_eventcodes on logging_events.eventid=logging_eventcodes.eventid full outer join logging_statuscodes on logging_events.statusid=logging_statuscodes.statusid where logging_events.key is not null order by timecode asc');
+      const result = await client.query('select logging_events.key, sessionid, timecode, logging_events.eventid, logging_eventcodes.description as eventdescription, logging_events.statusid, logging_statuscodes.description as statusdescription, ticketid from logging_events join logging_eventcodes on logging_events.eventid=logging_eventcodes.eventid full outer join logging_statuscodes on logging_events.statusid=logging_statuscodes.statusid where logging_events.key is not null order by timecode desc');
       const results = { 
         'results': (result) ? result.rows : null
       };
